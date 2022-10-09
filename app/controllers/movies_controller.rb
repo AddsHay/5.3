2467008b58fab033f9a80c33c801c40029d38e46
Adrian Haynes <adhay88@berkeley.edu>
2022-10-09 20:09:19 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,47 +1,60 @@
 class MoviesController < ApplicationController
 
-    def show
-      id = params[:id] # retrieve movie ID from URI route
-      @movie = Movie.find(id) # look up movie by unique ID
-      # will render app/views/movies/show.<extension> by default
-    end
-  
-    def index
-      @movies = Movie.all
-    end
-  
-    def new
-      # default: render 'new' template
-    end
-  
-    def create
-      @movie = Movie.create!(movie_params)
-      flash[:notice] = "#{@movie.title} was successfully created."
-      redirect_to movies_path
-    end
-  
-    def edit
-      @movie = Movie.find params[:id]
-    end
-  
-    def update
-      @movie = Movie.find params[:id]
-      @movie.update_attributes!(movie_params)
-      flash[:notice] = "#{@movie.title} was successfully updated."
-      redirect_to movie_path(@movie)
-    end
-  
-    def destroy
-      @movie = Movie.find(params[:id])
-      @movie.destroy
-      flash[:notice] = "Movie '#{@movie.title}' deleted."
-      redirect_to movies_path
-    end
-  
-    private
-    # Making "internal" methods private is not required, but is a common practice.
-    # This helps make clear which methods respond to requests, and which ones do not.
-    def movie_params
-      params.require(:movie).permit(:title, :rating, :description, :release_date)
-    end
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.<extension> by default
   end
+
+  def index
+    sort = params[:sort] || session[:sort]
+
+    @movies = Movie.all
+    @all_ratings = Movie.all_ratings
+    @ratings_to_show = params[:ratings] || session[:ratings] || {}
+    if @ratings_to_show == {}
+      @ratings_to_show = Hash[@all_ratings.map {|rating| [rating, 1]}]
+    end
+
+    if params[:sort] != session[:sort] or params[:ratings] != session[:ratings]
+      session[:sort] = sort
+      session[:ratings] = @ratings_to_show
+    end
+    @movies = Movie.where(rating: @ratings_to_show.keys).order(sort)
+  end
+
+  def new
+    # default: render 'new' template
+  end
+
+  def create
+    @movie = Movie.create!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
+
+  private
+  # Making "internal" methods private is not required, but is a common practice.
+  # This helps make clear which methods respond to requests, and which ones do not.
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
+end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,12 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings
+    ['G','PG','PG-13','R']
+  end
+  def self.with_ratings(ratings_list)
+    # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
+    #  movies with those ratings
+    return Movie.all 
+    # if ratings_list is nil, retrieve ALL movies
+  end
 end
+Footer
